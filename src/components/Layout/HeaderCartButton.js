f634c65a60d863926ddedac2136386f4e9fb7a84
UpendraSinghBhadouria <upendrasinghbhadouria75@gmail.com>
2023-09-23 23:25:28 +0530

    bump out button animation added conditionally

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import cartContext from '../../store/cart-context'
 
 const HeaderCartButton = (props) => {
 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     const cartCtx = useContext(cartContext);
-    const totalNumberOfItems = cartCtx.items.reduce((acc,item)=>{
+    const {items} = cartCtx;
+    const totalNumberOfItems = items.reduce((acc,item)=>{
         return acc+item.amount;
     },0)
 
+    const cartBtns = `${classes.button} ${btnIsHighlighted ? classes.bump: ''}`;
+
+    useEffect(()=>{
+        if(items.length===0){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(()=>{
+            setBtnIsHighlighted(false);
+        },300)
+        return ()=>{
+            clearTimeout(timer);
+        }
+    },[items])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={cartBtns} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
